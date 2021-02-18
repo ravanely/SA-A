d726ee30753dbc2c3359e9a3b589f34fb38ecaba
nague <nagueravanely@gmail.com>
2021-02-18 16:33:35 +0100

    login user, register new user, profile update and change password added successfuly

---
+++ frontend/src/components/route/ProtectedRoute.js
@@ -0,0 +1,27 @@
+import React, {Fragment} from 'react';
+import {useSelector} from "react-redux";
+import {Redirect, Route} from "react-router-dom";
+
+const ProtectedRoute = ({component: Component, ...rest}) => {
+
+    const {isAuthenticated, loading, user} = useSelector(state => state.auth)
+
+    return (
+        <Fragment>
+            {loading === false && (
+                <Route
+                    {...rest}
+                    render={props => {
+                        if (isAuthenticated === false) {
+                            return <Redirect to='/login'/>
+                        }
+
+                        return <Component {...props}/>
+                    }}
+                />
+            )}
+        </Fragment>
+    )
+}
+
+export default ProtectedRoute
---
+++ frontend/src/store.js
@@ -0,0 +1,20 @@
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import thunk from "redux-thunk";
+import {composeWithDevTools} from "redux-devtools-extension";
+import {productDetailsReducer, productsReducer} from "./reducers/productReducers";
+import {authReducer, userReducer} from "./reducers/userReducers";
+
+const reducer = combineReducers({
+    products: productsReducer,
+    productDetails: productDetailsReducer,
+    auth: authReducer,
+    user: userReducer
+})
+
+let initialState = {}
+
+const middleware = [thunk];
+
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+
+export default store
